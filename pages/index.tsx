61dbc4928a2f2227474093806f1634dbed6da4bc
MaxiAragon <jose.maximiliano.aragon@gmail.com>
2022-11-09 09:30:48 -0300

    add material ui and template dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,114 +1,208 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {
-  StreamClient,
-  Stream,
-  CreateParams,
-  CreateMultiParams,
-  WithdrawParams,
-  TransferParams,
-  TopupParams,
-  CancelParams,
-  GetAllParams,
-  StreamDirection,
-  StreamType,
-  Cluster,
-  TxResponse,
-  CreateResponse,
-  BN,
-  getBN,
-  getNumberFromBN,
-} from "@streamflow/stream";
-import { useEffect } from 'react';
-import PhantomWalletFactory from '../src/wallets/phantom';
-import { Keypair } from '@solana/web3.js';
-import { generateKeyPair, generateKeyPairSync } from 'crypto';
-import { web3 } from '@project-serum/anchor';
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
-export default function Home() {
-  
-  const streamClient = new StreamClient(
-    "https://api.devnet.solana.com", //https://streamfl-streamfl-1cdd.devnet.rpcpool.com
-    Cluster.Devnet,
-    "confirmed"
-  )
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-  
-  const handleCreateStream = async () => {
-    const phantomWallet = await PhantomWalletFactory.createPhantomWallet();
+const drawerWidth: number = 240;
 
-    console.log("phantomWallet", phantomWallet.publicKey?.toBase58())
-    try {
-      const streams = await streamClient.get({
-        wallet: phantomWallet.publicKey, // Wallet signing the transaction.
-        type: StreamType.All, // (optional) Type, default is StreamType.All
-        direction: StreamDirection.All, // (optional) Direction, default is StreamDirection.All)
-      });
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
 
-      const createStreamParams: CreateParams = {
-        sender:  phantomWallet, // Wallet/Keypair signing the transaction, creating and sending the stream.
-        recipient: "A3gWSs3vB6T1hwbEP5ENJgnydBJzH3TL1QjPWASYkDbK", // Solana recipient address.
-        mint: "HaWnUM4z1Y3HMZ7BbQhnY3DGMLE8dZeBnLJUCKxf1fcT", // SPL Token mint.
-        start: 16678666924, // Timestamp (in seconds) when the stream/token vesting starts.
-        depositedAmount: getBN(100, 9), // depositing 100 tokens with 9 decimals mint.
-        period: 1, // Time step (period) in seconds per which the unlocking occurs.
-        cliff: 16678669124, // Vesting contract "cliff" timestamp in seconds.
-        cliffAmount: new BN(10), // Amount unlocked at the "cliff" timestamp.
-        amountPerPeriod: getBN(5, 9), // Release rate: how many tokens are unlocked per each period.
-        name: "Transfer to Jane Doe.", // The stream name or subject.
-        canTopup: false, // setting to FALSE will effectively create a vesting contract.
-        cancelableBySender: true, // Whether or not sender can cancel the stream.
-        cancelableByRecipient: false, // Whether or not recipient can cancel the stream.
-        transferableBySender: true, // Whether or not sender can transfer the stream.
-        transferableByRecipient: false, // Whether or not recipient can transfer the stream.
-        automaticWithdrawal: true, // Whether or not a 3rd party (e.g. cron job, "cranker") can initiate a token withdraw/transfer.
-        withdrawalFrequency: 10, // Relevant when automatic withdrawal is enabled. If greater than 0 our withdrawor will take care of withdrawals. If equal to 0 our withdrawor will skip, but everyone else can initiate withdrawals.
-        partner: null, //  (optional) Partner's wallet address (string | null).
-      };
-    
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
-      console.log("streams", streams)
-      const response = await streamClient.create({
-        ...createStreamParams,
-      })
-      console.log(response)
-    } catch(e) {
-      console.error(e);
-    }
-  }
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Streamflow test</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Streamflow test app!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by loging with phantom plugin
-        </p>
-        <button onClick={handleCreateStream}>
-          Phantom
-        </button>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/streamflow-finance/js-sdk"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {"mainListItems"}
+            <Divider sx={{ my: 1 }} />
+            {"secondaryListItems"}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
         >
-          Powered by
-          <code style={{ marginLeft: 3 }}>@streamflow/stream</code>
-        </a>
-      </footer>
-    </div>
-  )
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Chart */}
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  {"<Chart />"}
+                </Paper>
+              </Grid>
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  {"<Deposits />"}
+                </Paper>
+              </Grid>
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                  {"<Orders />"}
+                </Paper>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function Dashboard() {
+  return <DashboardContent />;
 }
